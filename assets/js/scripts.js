fc40396b86c5338c86474e3d4d1cb312d8cc26a6
Messias Junior <messiaslima.03@gmail.com>
2023-03-20 17:26:14 +0000

    Updated from Publii

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -198,6 +198,8 @@ window.addEventListener('scroll', function (e) {
         if (config.mobileMenuExpandableSubmenus) {
             wrapSubmenusIntoContainer(menuWrapper);
             initToggleSubmenu(menuWrapper);
+        } else {
+            setAriaForSubmenus(menuWrapper);
         }
 
         // Init button events
@@ -255,6 +257,8 @@ window.addEventListener('scroll', function (e) {
         if (config.mobileMenuExpandableSubmenus) {
             wrapSubmenusIntoContainer(menuWrapper);
             initToggleSubmenu(menuWrapper);
+        } else {
+            setAriaForSubmenus(menuWrapper);
         }
 
         // Menu events
@@ -280,6 +284,17 @@ window.addEventListener('scroll', function (e) {
             button.setAttribute(config.ariaButtonAttribute, button.classList.contains(config.openedMenuClass));
             document.documentElement.classList.toggle(config.noScrollClass);
         });
+    }
+
+    /**
+     * Set aria-hidden="false" for submenus
+     */
+    function setAriaForSubmenus (menuWrapper) {
+        var submenus = menuWrapper.querySelectorAll(config.submenuSelector);
+
+        for (var i = 0; i < submenus.length; i++) {
+            submenus[i].setAttribute('aria-hidden', false);
+        }
     }
 
     /**
@@ -512,17 +527,19 @@ window.addEventListener('scroll', function (e) {
 // Load search input area
 var searchButton = document.querySelector('.js-search-btn');
     searchOverlay = document.querySelector('.js-search-overlay');
-    searchInput = document.querySelector('.js-search-input');
+    searchInput = document.querySelector('[type="search"]');
 
 if (searchButton) {
     searchButton.addEventListener('click', function () {        
         searchOverlay.classList.toggle('expanded');
        
-        setTimeout(function() {
-            if (searchOverlay.classList.contains('expanded')) {
-                searchInput.focus();
-            }
-        }, 60);        
+        if (searchInput) {
+            setTimeout(function() {
+                if (searchOverlay.classList.contains('expanded')) {
+                    searchInput.focus();
+                }
+            }, 60);  
+        }      
     });
 
     searchOverlay.addEventListener('click', function (e) {
@@ -538,3 +555,43 @@ if (searchButton) {
     });
 }
 
+
+// Responsive embeds script
+(function () {
+	let wrappers = document.querySelectorAll('.post__video, .post__iframe');
+
+	for (let i = 0; i < wrappers.length; i++) {
+		let embed = wrappers[i].querySelector('iframe, embed, video, object');
+
+		if (!embed) {
+			continue;
+		}
+
+        if (embed.getAttribute('data-responsive') === 'false') {
+            continue;
+        }
+
+		let w = embed.getAttribute('width');
+		let h = embed.getAttribute('height');
+		let ratio = false;
+
+		if (!w || !h) {
+			continue;
+		}
+		
+		if (w.indexOf('%') > -1 && h.indexOf('%') > -1) { // percentage mode
+			w = parseFloat(w.replace('%', ''));
+			h = parseFloat(h.replace('%', ''));
+			ratio = h / w;
+		} else if (w.indexOf('%') === -1 && h.indexOf('%') === -1) { // pixels mode
+			w = parseInt(w, 10);
+			h = parseInt(h, 10);
+			ratio = h / w;
+		}
+
+		if (ratio !== false) {
+			let ratioValue = (ratio * 100) + '%';
+			wrappers[i].setAttribute('style', '--embed-aspect-ratio:' + ratioValue);
+		}
+	}
+})();
