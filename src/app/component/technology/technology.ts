13a2790abbacf25ebabce1b86ae4f63b79cfbddf
Messias Junior <messiaslima.03@gmail.com>
2025-07-02 13:25:03 +0100

    tech section

---
+++ src/app/component/technology/technology.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technology',
+  imports: [],
+  templateUrl: './technology.html',
+  styleUrl: './technology.scss'
+})
+export class Technology {
+
+}
--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../component/hero/hero';
 import { Services } from '../../component/services/services';
+import { Technology } from '../../component/technology/technology';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Services],
+  imports: [Hero, Services, Technology],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
