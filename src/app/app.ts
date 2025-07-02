fa0ba2f17d75c7a2675b3efb11179692f60ad54f
Messias Junior <messiaslima.03@gmail.com>
2025-07-02 12:32:55 +0100

    footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav } from './component/nav/nav';
+import { Footer } from './component/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav],
+  imports: [RouterOutlet, Nav, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
