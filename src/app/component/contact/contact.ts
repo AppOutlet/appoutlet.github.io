a7ad6c60a66afc11e814aabcb08f03fb7b8734a1
Messias Junior <messiaslima.03@gmail.com>
2025-07-07 19:30:55 +0100

    social links

--- src/app/component/contact/contact.ts
+++ src/app/component/contact/contact.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import { title } from 'node:process';
 
 @Component({
   selector: 'app-contact',
   imports: [],
   templateUrl: './contact.html',
-  styleUrl: './contact.scss'
+  styleUrl: './contact.scss',
 })
 export class Contact {
+  processSteps = [
+    {
+      title: 'Initial Contact',
+      description: 'Reach out via email or social media',
+    },
+    {
+      title: 'Discovery Call',
+      description: 'We discuss your vision and requirements',
+    },
+    {
+      title: 'Project Proposal',
+      description: 'Detailed timeline, scope, and pricing',
+    },
+    {
+      title: 'Development & Launch',
+      description: 'Agile development with regular updates',
+    },
+  ];
 
+  socialLinks = [
+    {
+      title: 'X (Twitter)',
+      url: 'https://x.com/AppOutletTeam',
+      icon: 'ph-x-logo',
+    },
+    {
+      title: 'Threads',
+      url: 'https://x.com/appoutlet',
+      icon: 'ph-threads-logo',
+    },
+    {
+      title: 'Mastodon',
+      url: 'https://mastodon.social/@AppOutlet',
+      icon: 'ph-mastodon-logo',
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/company/appoutlet/',
+      icon: 'ph-linkedin-logo',
+    },
+  ];
 }
