d38722a49176c82e5f9fbd2792b20fc49c69e89e
Messias Junior <messiaslima.03@gmail.com>
2025-07-08 18:26:44 +0100

    Add language selection dropdown and update language handling

--- src/app/component/nav/nav.ts
+++ src/app/component/nav/nav.ts
@@ -1,15 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+declare var window: any; // To avoid TypeScript errors for window usage
 
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.html',
-  styleUrl: './nav.scss'
+  styleUrl: './nav.scss',
 })
-export class Nav {
+export class Nav implements AfterViewInit {
   isActive = false;
 
   toogleActive() {
     this.isActive = !this.isActive;
   }
+
+  defaultLanguage: Language = { name: '🇬🇧 Eng', baseHref: '' };
+  availableTranslation: Language[] = [{ name: '🇧🇷 Por', baseHref: '/pt/' }];
+  selectedLanguage: Language = this.defaultLanguage;
+
+  ngAfterViewInit() {
+    this.selectedLanguage = this.updateSelectedLanguage();
+  }
+
+  private updateSelectedLanguage(): Language {
+    const match = this.availableTranslation.find((lang) =>
+      window.location.href.includes(lang.baseHref)
+    );
+    return match || this.defaultLanguage;
+  }
+}
+
+interface Language {
+  name: string;
+  baseHref: string;
 }
