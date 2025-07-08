24c1338e6579d7593b310e7e588d41da18ab0fcb
Messias Junior <messiaslima.03@gmail.com>
2025-07-08 10:33:53 +0100

    footer 1

--- src/app/component/footer/footer.ts
+++ src/app/component/footer/footer.ts
@@ -4,8 +4,40 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
-
+  year: number = new Date().getFullYear();
+  socialLinks = [
+    {
+      title: 'X (Twitter)',
+      url: 'https://x.com/AppOutletTeam',
+      icon: 'ph-x-logo',
+    },
+    {
+      title: 'Threads',
+      url: 'https://x.com/appoutlet',
+      icon: 'ph-threads-logo',
+    },
+    {
+      title: 'Mastodon',
+      url: 'https://mastodon.social/@AppOutlet',
+      icon: 'ph-mastodon-logo',
+    },
+    {
+      title: 'LinkedIn',
+      url: 'https://www.linkedin.com/company/appoutlet/',
+      icon: 'ph-linkedin-logo',
+    },
+    {
+      title: 'Instagram',
+      url: 'https://www.instagram.com/app.outlet/',
+      icon: 'ph-instagram-logo',
+    },
+    {
+      title: 'YouTube',
+      url: 'https://www.youtube.com/@AppOutletTeam',
+      icon: 'ph-youtube-logo',
+    },
+  ];
 }
