55edd1079be2cb622c5f16a3b3157464b93ae6f2
AppOutletTeam <team.appoutlet@gmail.com>
2025-08-29 17:16:48 +0100

    adjust navigation menus

--- src/app/component/navigation/navigation.ts
+++ src/app/component/navigation/navigation.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from "@angular/material/menu";
 
 @Component({
   selector: 'app-navigation',
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, MatMenuModule],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
