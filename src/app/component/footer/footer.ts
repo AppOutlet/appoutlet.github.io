a15b98ba2a2eb29b58a844d2f4e5a71e71bb19de
AppOutletTeam <team.appoutlet@gmail.com>
2025-08-29 16:05:19 +0100

    clean application

---
+++ src/app/component/footer/footer.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss'
+})
+export class Footer {
+
+}
