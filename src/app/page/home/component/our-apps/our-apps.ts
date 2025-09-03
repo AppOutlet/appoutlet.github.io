7325a0df786893909e725734d1beb13f5ceaff5d
AppOutletTeam <team.appoutlet@gmail.com>
2025-09-03 09:53:20 +0100

    Create our apps

---
+++ src/app/page/home/component/our-apps/our-apps.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-our-apps',
+  imports: [],
+  templateUrl: './our-apps.html',
+  styleUrl: './our-apps.scss'
+})
+export class OurApps {
+
+}
--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Hero } from './component/hero/hero';
 import { Services } from './component/services/services';
+import { OurApps } from './component/our-apps/our-apps';
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Services],
+  imports: [Hero, Services, OurApps],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
