067c99d8f9781d6cf4e593e4f1d15baeaa43eb08
AppOutletTeam <team.appoutlet@gmail.com>
2025-09-16 12:01:01 +0100

    add contact component to home page

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -3,11 +3,12 @@ import { LucideAngularModule } from 'lucide-angular';
 import { Navigation } from '../../component/navigation/navigation';
 import { Footer } from '../../component/footer/footer';
 import { Hero } from '../../component/hero/hero';
+import { Contact } from '../../component/contact/contact';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [LucideAngularModule, Navigation, Footer, Hero],
+  imports: [LucideAngularModule, Navigation, Footer, Hero, Contact],
   templateUrl: './home.html',
 })
 export class Home {
