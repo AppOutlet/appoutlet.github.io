bcb7f7844c2659fca7c636d12e8ed85a0fbfdf22
AppOutletTeam <team.appoutlet@gmail.com>
2025-09-18 11:17:37 +0100

    create our apps

--- src/app/page/home/home.ts
+++ src/app/page/home/home.ts
@@ -4,11 +4,12 @@ import { Footer } from '../../component/footer/footer';
 import { Hero } from '../../component/hero/hero';
 import { Contact } from '../../component/contact/contact';
 import { Services } from '../../component/services/services';
+import { OurApps } from '../../component/our-apps/our-apps';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Navigation, Footer, Hero, Contact, Services],
+  imports: [Navigation, Footer, Hero, Contact, Services, OurApps],
   templateUrl: './home.html',
 })
 export class Home {
