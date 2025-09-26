5c70d02bbbbbfd55d44eb3291b794ace85db07ad
AppOutletTeam <team.appoutlet@gmail.com>
2025-09-26 17:11:46 +0100

    [dyad] Add language switcher to navigation - wrote 5 file(s)

--- src/app/component/navigation/navigation.ts
+++ src/app/component/navigation/navigation.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LanguageSwitcher } from '../language-switcher/language-switcher';
 
 @Component({
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LanguageSwitcher],
   templateUrl: './navigation.html',
 })
 export class Navigation {
